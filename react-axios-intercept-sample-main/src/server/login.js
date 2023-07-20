81a3143a0dfa2f2464511fc29a447996c1e12067
sajnaa <sajnaparveen05@gmail.com>
2023-07-20 17:43:12 +0530

    workinginprogress

--- react-axios-intercept-sample-main/src/server/login.js
+++
@@ -1,9 +0,0 @@
-import api, { resetRetry } from '../api/'
-
-export default async function login({ id, passwd }) {
-
-    resetRetry()
-
-    return api.post('/login', { id: id, pwd: passwd })
-
-}
--- react-axios-intercept-sample-main/src/server/refresh.js
+++
@@ -1,9 +0,0 @@
-import api, { resetRetry } from '../api/'
-
-export default async function refresh({ rtoken }) {
-
-    resetRetry()
-
-    return api.post('/refresh', {rtoken: rtoken})
-
-}
